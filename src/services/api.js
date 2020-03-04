474c5fd19d755f9dc89eaed44e314fa432f18965
Rodrigo Rodrigues de Oliveira <rroliveira@hospitalonix.local>
2020-03-04 15:13:51 -0300

    Alteração URL API

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseURL: 'https://rroweek-backend.herokuapp.com'
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: 'https://roliveirabox.herokuapp.com/'
+    //baseURL: process.env.REACT_APP_API_URL
     //baseURL: 'http://localhost:3333'
 });
 
